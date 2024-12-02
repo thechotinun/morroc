70e88a89188cbbe53aaf8caab7490bfe83934b3d
thechotinun <thechotinun@gmail.com>
2024-12-02 14:39:58 +0700

    add config

---
+++ src/main/java/com/morroc/v1/configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package com.morroc.v1.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
